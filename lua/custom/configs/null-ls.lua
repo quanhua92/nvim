d80f06173ebffc35c182d4370e734191c3f95c41
Quan Hua <quanhua92@gmail.com>
2023-05-21 01:26:22 +0700

    lspconfig null-ls format on save

---
+++ lua/custom/configs/null-ls.lua
@@ -0,0 +1,39 @@
+local null_ls = require "null-ls"
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local b = null_ls.builtins
+
+local sources = {
+  -- rust
+  b.formatting.rustfmt,
+
+  -- webdev
+  b.formatting.prettierd,
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- Shell
+  b.formatting.shfmt,
+  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
+}
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,53 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function ()
+        require("custom.configs.null-ls")
+      end
+    }
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "typescript-language-server",
+        "prettierd",
+        "stylua"
+      }
+    }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+
+        -- web dev
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+
+        -- rust
+        "rust",
+
+        -- low level
+        "c",
+      }
+    }
+  }
+}
+
+return plugins

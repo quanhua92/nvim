af5837fb049f42afa62ff52a5ea65b175e9e58f6
Quan Hua <quanhua92@gmail.com>
2023-05-23 14:41:59 +0700

    add install node in script

--- install-neovim-docker.sh
+++ install-neovim-docker.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+apt update && apt install unzip
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
+
 cd /tmp/
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
+rm -rf /squashfs-root
 mv squashfs-root /
+rm /usr/bin/vim
+rm /usr/bin/nvim
 ln -s /squashfs-root/AppRun /usr/bin/vim
 ln -s /squashfs-root/AppRun /usr/bin/nvim

b0592aee3a5d305bb465f0032a72c1141cfcbd98
Quan Hua <quanhua92@gmail.com>
2023-05-23 11:18:14 +0700

    disable hlsearch

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,6 +5,9 @@ vim.keymap.set("n", "ZZ", "zz<cr>")
 -- set relative number
 vim.o.relativenumber = true
 
+-- set highlight search
+vim.o.hlsearch = false
+
 -- vim-visual-multi
 vim.g.VM_maps = {
   ["Find Under"] = "<C-d>",
